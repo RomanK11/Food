36038556d4e18cac66b9811e75e1e4f98c6caab8
Roman <romankorovin032@gmail.com>
2023-10-06 17:25:51 +0300

    проект добавлен

---
+++ src/main/java/com/freeCinema/cinema/controller/AuthController.java
@@ -0,0 +1,37 @@
+package com.freeCinema.cinema.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import com.freeCinema.cinema.model.User;
+import com.freeCinema.cinema.service.AuthService;
+
+@Controller
+public class AuthController {
+    private final AuthService authService;
+
+    @Autowired
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @GetMapping("/login")
+    public String getLogin() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String authenticate(@RequestParam String username, @RequestParam String password) {
+        if (authService.authenticate(username, password)) {
+            return "loginJJ";
+        } else {
+            return "error";
+        }
+    }
+
+    @ModelAttribute(name = "user")
+    public User user() {
+        return new User();
+    }
+}
---
+++ src/main/java/com/freeCinema/cinema/model/User.java
@@ -0,0 +1,22 @@
+package com.freeCinema.cinema.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table("USERS")
+public class User {
+    @Id
+    private Long id;
+
+    private String username;
+
+    private String password;
+}
